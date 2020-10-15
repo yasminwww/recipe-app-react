4e3d87e18615c7bfa7a2248f03d7b20f2362b674
Yasmin <ym222bs@student.lnu.se>
2020-10-15 15:23:35 +0200

    finally css for about page

--- src/components/About.js
+++ src/components/About.js
@@ -4,24 +4,30 @@ import styled from 'styled-components'
 import spices from '../images/spices.jpg'
 
 const Wrapper = styled.div`
-  background: url(${spices}) no-repeat;
-  display: flex;
-  height: 100%;
-  flex-direction: column;
-  justify-items: center;
-  align-items: center;
-
-  .content {
-    object-fit: cover;
+  .so {
     display: flex;
+    /* height: 100%; */
     flex-direction: column;
-    justify-items: center;
-    align-items: center;
-    padding: 15px;
-    height: 600px;
+
+    /* justify-items: center;
+    align-items: center; */
+    height: 100vh;
+  }
+  img {
+    display: block;
+    margin-left: auto;
+    margin-right: auto;
+    width: 70%;
   }
 
-  /*
+  .content {
+    /* flex: 1; */
+    position: absolute;
+    top: 40%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+  }
+
   h2,
   p {
     line-height: 30px;
@@ -30,25 +36,25 @@ const Wrapper = styled.div`
     font-weight: bold;
     justify-items: start;
   }
-  p {
-    width: 50%;
-  } */
 `
 
 const About = () => {
   return (
     <Wrapper>
-      <div className='content'>
-        <h2>For those who are passionate about cooking.</h2>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum.
-        </p>
+      <div className='so'>
+        <img src={spices} alt='spices' />
+        <div className='content'>
+          <h2>For those who are passionate about cooking.</h2>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.
+          </p>
+        </div>
       </div>
     </Wrapper>
   )
